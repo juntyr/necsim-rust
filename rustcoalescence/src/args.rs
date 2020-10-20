5c0bf7ad742157ed8a3de51bae6b08f8e7a8c835
Moritz Langenstein <moritz.langenstein17@imperial.ac.uk>
2020-10-20 10:37:21 +0000

    (ml5717) Refactored rustcoalescence argument parsing

---
+++ rustcoalescence/src/args.rs
@@ -0,0 +1,32 @@
+use derive_getters::Getters;
+use structopt::clap::arg_enum;
+use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Algorithm {
+        Classical,
+        Gillespie
+    }
+}
+
+#[derive(Debug, StructOpt, Getters)]
+pub struct CommandLineArguments {
+    #[structopt(parse(from_os_str))]
+    habitat_map: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    dispersal_map: std::path::PathBuf,
+    #[structopt(long = "speciation")]
+    speciation_probability_per_generation: f64,
+    #[structopt(long = "sample")]
+    sample_percentage: f64,
+    #[structopt(long)]
+    seed: u64,
+    #[structopt(
+        possible_values = &Algorithm::variants(),
+        case_insensitive = true,
+        default_value = "Classical",
+        long = "algorithm"
+    )]
+    algorithm: Algorithm,
+}
---
+++ rustcoalescence/src/maps.rs
@@ -0,0 +1,44 @@
+use std::path::PathBuf;
+
+use anyhow::{Context, Result};
+use array2d::Array2D;
+use cast::{Error, From as _From};
+
+use super::gdal::load_map_f64_from_gdal_raster;
+
+pub fn load_dispersal_map(path: &PathBuf) -> Result<Array2D<f64>> {
+    load_map_f64_from_gdal_raster(path).context("Failed to load the dispersal map")
+}
+
+pub fn load_habitat_map(path: &PathBuf, dispersal: &Array2D<f64>) -> Result<Array2D<u32>> {
+    let habitat_f64 =
+        load_map_f64_from_gdal_raster(path).context("Failed to load the habitat map")?;
+
+    let mut habitat: Array2D<u32> =
+        Array2D::filled_with(0, habitat_f64.num_rows(), habitat_f64.num_columns());
+
+    for y in 0..habitat_f64.num_rows() {
+        for x in 0..habitat_f64.num_columns() {
+            let h_f64 = habitat_f64[(y, x)];
+
+            habitat[(y, x)] = if h_f64 < 0.0_f64 {
+                Err(Error::Underflow)
+            } else if h_f64 < 1.0_f64 {
+                // If there is any dispersal from this location, it must be habitat
+                if dispersal
+                    .row_iter(y * habitat.num_columns() + x)
+                    .any(|p| *p > 0.0_f64)
+                {
+                    Ok(1)
+                } else {
+                    Ok(0)
+                }
+            } else {
+                u32::cast(h_f64)
+            }
+            .context("Failed to interpret the habitat map as u32")?;
+        }
+    }
+
+    Ok(habitat)
+}
