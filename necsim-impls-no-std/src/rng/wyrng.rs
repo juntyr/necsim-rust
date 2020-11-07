e84a7eafe6d5462777e680bd63ccedbc5f656613
ml5717 <ml5717@ic.ac.uk>
2020-11-07 12:24:05 +0000

    (ml5717) Fixed U(0.0, 1.0) generation from random u64

--- necsim-impls-no-std/src/rng/wyrng.rs
+++ necsim-impls-no-std/src/rng/wyrng.rs
@@ -6,10 +6,11 @@ pub struct WyRng(WyRand);
 #[contract_trait]
 impl necsim_core::rng::Core for WyRng {
     fn sample_uniform(&mut self) -> f64 {
+        // U(0.0, 1.0) generation from:
+        // http://prng.di.unimi.it -> Generating uniform doubles in the unit interval
         #[allow(clippy::cast_precision_loss)]
-        (self.0.generate_range(0_u64, u64::MAX) as f64)
-            / #[allow(clippy::cast_precision_loss)]
-            (u64::MAX as f64)
+        ((self.0.generate::<u64>() >> 11) as f64)
+            * f64::from_bits(0x3CA0_0000_0000_0000_u64) //0x1.0p-53
     }
 }
 
