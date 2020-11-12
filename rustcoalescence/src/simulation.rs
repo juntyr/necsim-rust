ee5017b9c58b30ea31c541ad1e916b1b4d3a9823
Moritz Langenstein <ml5717@ic.ac.uk>
2020-11-12 08:23:24 +0000

    (ml5717) Added feature-gates to rustcoalescence algorithms (clippy not yet fixed)

--- rustcoalescence/src/simulation.rs
+++ rustcoalescence/src/simulation.rs
@@ -1,17 +1,28 @@
 use anyhow::{Context, Result};
 use array2d::Array2D;
 
+#[cfg(feature = "necsim-classical")]
 use necsim_classical::ClassicalSimulation;
-use necsim_core::cogs::PrimeableRng;
-use necsim_core::reporter::Reporter;
+
+#[cfg(feature = "necsim-cuda")]
 use necsim_cuda::CudaSimulation;
+
+#[cfg(feature = "necsim-gillespie")]
 use necsim_gillespie::GillespieSimulation;
-use necsim_impls_no_std::cogs::habitat::in_memory::InMemoryHabitat;
-use necsim_impls_no_std::cogs::lineage_reference::in_memory::InMemoryLineageReference;
+
+#[cfg(feature = "necsim-skipping-gillespie")]
 use necsim_skipping_gillespie::SkippingGillespieSimulation;
 
+use necsim_core::cogs::PrimeableRng;
+use necsim_core::reporter::Reporter;
+use necsim_impls_no_std::cogs::habitat::in_memory::InMemoryHabitat;
+use necsim_impls_no_std::cogs::lineage_reference::in_memory::InMemoryLineageReference;
+
+#[allow(unused_imports)]
 use super::args::{Algorithm, CommandLineArguments};
 
+#[allow(unreachable_code)]
+#[allow(unused_variables)]
 pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
     args: &CommandLineArguments,
     habitat: &Array2D<u32>,
@@ -24,7 +35,8 @@ pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
         args.algorithm()
     );
 
-    match args.algorithm() {
+    let result: Result<(f64, u64)> = match args.algorithm() {
+        #[cfg(feature = "necsim-classical")]
         Algorithm::Classical => ClassicalSimulation::simulate(
             habitat,
             &dispersal,
@@ -33,6 +45,7 @@ pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
             rng,
             reporter,
         ),
+        #[cfg(feature = "necsim-gillespie")]
         Algorithm::Gillespie => GillespieSimulation::simulate(
             habitat,
             &dispersal,
@@ -41,6 +54,7 @@ pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
             rng,
             reporter,
         ),
+        #[cfg(feature = "necsim-skipping-gillespie")]
         Algorithm::SkippingGillespie => SkippingGillespieSimulation::simulate(
             habitat,
             &dispersal,
@@ -49,6 +63,7 @@ pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
             rng,
             reporter,
         ),
+        #[cfg(feature = "necsim-cuda")]
         Algorithm::CUDA => CudaSimulation::simulate(
             habitat,
             &dispersal,
@@ -57,8 +72,11 @@ pub fn simulate<G: PrimeableRng<InMemoryHabitat>>(
             rng,
             reporter,
         ),
-    }
-    .with_context(|| {
+        #[allow(unreachable_patterns)]
+        _ => anyhow::bail!("rustcoalescence does not support the selected algorithm"),
+    };
+
+    result.with_context(|| {
         format!(
             concat!(
                 "Failed to create the Simulation with the habitat ",
