8777dd4e362283a378f84d4bab5e01676e85e2b2
ml5717 <ml5717@ic.ac.uk>
2021-01-25 20:30:01 +0000

    (ml5717) Fixed CUDA algorithm launch

--- necsim/algorithms/cuda/src/simulate.rs
+++ necsim/algorithms/cuda/src/simulate.rs
@@ -51,7 +51,7 @@ pub fn simulate<
 >(
     stream: &Stream,
     kernel: &SimulationKernel<H, G, N, D, R, S, X, C, E, I, A, REPORT_SPECIATION, REPORT_DISPERSAL>,
-    task: (u32, GridSize, BlockSize),
+    config: (GridSize, BlockSize),
     mut simulation: Simulation<H, G, N, D, R, S, X, C, E, I, A>,
     mut individual_tasks: VecDeque<Lineage>,
     task_list: ValueBuffer<Lineage>,
@@ -76,7 +76,7 @@ pub fn simulate<
         kernel.get_global(&CString::new("global_steps_sum").unwrap())?;
     global_steps_sum_symbol.copy_from(&0_u64)?;
 
-    let (grid_amount, grid_size, block_size) = task;
+    let (grid_size, block_size) = config;
 
     let kernel = kernel
         .with_dimensions(grid_size, block_size, 0_u32)
@@ -93,57 +93,55 @@ pub fn simulate<
     // simulation, event analysis etc.
     if let Err(err) = simulation.lend_to_cuda_mut(|simulation_cuda_repr| {
         while !individual_tasks.is_empty() {
-            for _grid_id in 0..(grid_amount as usize) {
-                // Upload the new tasks from the front of the task queue
-                for task in task_list.iter_mut() {
-                    *task = individual_tasks.pop_front();
-                }
-
-                // Reset the individual duplication check bitmask
-                duplicate_individuals.set_all(false);
-
-                // Move the task list, event buffer and min speciation sample buffer to CUDA
-                let mut event_buffer_cuda = event_buffer.move_to_cuda()?;
-                let mut min_spec_sample_buffer_cuda = min_spec_sample_buffer.move_to_cuda()?;
-                let mut task_list_cuda = task_list.move_to_cuda()?;
-
-                // Launching kernels is unsafe since Rust cannot
-                // enforce safety across the foreign function
-                // CUDA-C language barrier
-                unsafe {
-                    kernel.launch(
-                        simulation_cuda_repr,
-                        task_list_cuda.as_mut(),
-                        event_buffer_cuda.as_mut(),
-                        min_spec_sample_buffer_cuda.as_mut(),
-                        max_steps,
-                    )?;
-                }
-
-                stream.synchronize()?;
-
-                min_spec_sample_buffer = min_spec_sample_buffer_cuda.move_to_host()?;
-                task_list = task_list_cuda.move_to_host()?;
-                event_buffer = event_buffer_cuda.move_to_host()?;
-
-                // Fetch the completion of the tasks
-                for (i, spec_sample) in min_spec_sample_buffer.iter_mut().enumerate() {
-                    if let Some(spec_sample) = spec_sample.take() {
-                        duplicate_individuals.set(i, !min_spec_samples.insert(spec_sample));
-                    }
-                }
-
-                // Fetch the completion of the tasks
-                for (i, task) in task_list.iter_mut().enumerate() {
-                    if let Some(task) = task.take() {
-                        if task.is_active() && !duplicate_individuals[i] {
-                            individual_tasks.push_back(task);
-                        }
-                    }
-                }
-
-                event_buffer.report_events(reporter);
+            // Upload the new tasks from the front of the task queue
+            for task in task_list.iter_mut() {
+                *task = individual_tasks.pop_front();
             }
+
+            // Reset the individual duplication check bitmask
+            duplicate_individuals.set_all(false);
+
+            // Move the task list, event buffer and min speciation sample buffer to CUDA
+            let mut event_buffer_cuda = event_buffer.move_to_cuda()?;
+            let mut min_spec_sample_buffer_cuda = min_spec_sample_buffer.move_to_cuda()?;
+            let mut task_list_cuda = task_list.move_to_cuda()?;
+
+            // Launching kernels is unsafe since Rust cannot
+            // enforce safety across the foreign function
+            // CUDA-C language barrier
+            unsafe {
+                kernel.launch(
+                    simulation_cuda_repr,
+                    task_list_cuda.as_mut(),
+                    event_buffer_cuda.as_mut(),
+                    min_spec_sample_buffer_cuda.as_mut(),
+                    max_steps,
+                )?;
+            }
+
+            stream.synchronize()?;
+
+            min_spec_sample_buffer = min_spec_sample_buffer_cuda.move_to_host()?;
+            task_list = task_list_cuda.move_to_host()?;
+            event_buffer = event_buffer_cuda.move_to_host()?;
+
+            // Fetch the completion of the tasks
+            for (i, spec_sample) in min_spec_sample_buffer.iter_mut().enumerate() {
+                if let Some(spec_sample) = spec_sample.take() {
+                    duplicate_individuals.set(i, !min_spec_samples.insert(spec_sample));
+                }
+            }
+
+            // Fetch the completion of the tasks
+            for (i, task) in task_list.iter_mut().enumerate() {
+                if let Some(task) = task.take() {
+                    if task.is_active() && !duplicate_individuals[i] {
+                        individual_tasks.push_back(task);
+                    }
+                }
+            }
+
+            event_buffer.report_events(reporter);
         }
 
         Ok(())
