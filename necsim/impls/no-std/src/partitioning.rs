bd9f4242fbfb25187557f73d3bfb19f41bd8bbc3
Moritz Langenstein <ml5717@ic.ac.uk>
2021-02-20 17:49:00 +0000

    (ml5717) Added migrate_individuals method LocalPartition

--- necsim/impls/no-std/src/partitioning.rs
+++ necsim/impls/no-std/src/partitioning.rs
@@ -1,6 +1,7 @@
+use alloc::vec::Vec;
 use core::num::NonZeroU32;
 
-use necsim_core::reporter::Reporter;
+use necsim_core::{lineage::MigratingLineage, reporter::Reporter};
 
 use crate::reporter::{GuardedReporter, ReporterContext};
 
@@ -33,6 +34,7 @@ pub trait Partitioning: Sized {
 #[contract_trait]
 pub trait LocalPartition<P: ReporterContext>: Sized {
     type Reporter: Reporter;
+    type ImmigrantIterator<'a>: Iterator<Item = MigratingLineage>;
 
     fn get_reporter(&mut self) -> &mut Self::Reporter;
 
@@ -50,17 +52,24 @@ pub trait LocalPartition<P: ReporterContext>: Sized {
     )]
     fn get_number_of_partitions(&self) -> NonZeroU32;
 
-    fn reduce_global_time_steps(&self, local_time: f64, local_steps: u64) -> (f64, u64);
+    fn migrate_individuals<E: Iterator<Item = (u32, MigratingLineage)>>(
+        &mut self,
+        emigrants: &mut E,
+    ) -> Self::ImmigrantIterator<'_>;
 
     fn wait_for_termination(&mut self) -> bool;
+
+    fn reduce_global_time_steps(&self, local_time: f64, local_steps: u64) -> (f64, u64);
 }
 
 pub struct MonolithicLocalPartition<P: ReporterContext> {
     reporter: GuardedReporter<P::Reporter, P::Finaliser>,
+    loopback: Vec<MigratingLineage>,
 }
 
 #[contract_trait]
 impl<P: ReporterContext> LocalPartition<P> for MonolithicLocalPartition<P> {
+    type ImmigrantIterator<'a> = ImmigrantPopIterator<'a>;
     type Reporter = P::Reporter;
 
     fn get_reporter(&mut self) -> &mut Self::Reporter {
@@ -79,12 +88,41 @@ impl<P: ReporterContext> LocalPartition<P> for MonolithicLocalPartition<P> {
         unsafe { NonZeroU32::new_unchecked(1) }
     }
 
-    fn reduce_global_time_steps(&self, local_time: f64, local_steps: u64) -> (f64, u64) {
-        (local_time, local_steps)
+    fn migrate_individuals<E: Iterator<Item = (u32, MigratingLineage)>>(
+        &mut self,
+        emigrants: &mut E,
+    ) -> Self::ImmigrantIterator<'_> {
+        for (_, emigrant) in emigrants {
+            self.loopback.push(emigrant);
+        }
+
+        ImmigrantPopIterator::new(&mut self.loopback)
     }
 
     fn wait_for_termination(&mut self) -> bool {
         false
+    }
+
+    fn reduce_global_time_steps(&self, local_time: f64, local_steps: u64) -> (f64, u64) {
+        (local_time, local_steps)
+    }
+}
+
+pub struct ImmigrantPopIterator<'i> {
+    immigrants: &'i mut Vec<MigratingLineage>,
+}
+
+impl<'i> ImmigrantPopIterator<'i> {
+    fn new(immigrants: &'i mut Vec<MigratingLineage>) -> Self {
+        Self { immigrants }
+    }
+}
+
+impl<'i> Iterator for ImmigrantPopIterator<'i> {
+    type Item = MigratingLineage;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.immigrants.pop()
     }
 }
 
@@ -92,6 +130,7 @@ impl<P: ReporterContext> MonolithicLocalPartition<P> {
     pub fn from_reporter(reporter_guard: GuardedReporter<P::Reporter, P::Finaliser>) -> Self {
         Self {
             reporter: reporter_guard,
+            loopback: Vec::new(),
         }
     }
 }
