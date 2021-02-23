c2cb06640455e4eb4da952342a4601387d0699a0
Moritz Langenstein <ml5717@ic.ac.uk>
2021-02-23 20:58:18 +0000

    (ml5717) Added EqualArea (Z-Order) and Radial decomposition

--- necsim/impls/no-std/src/decomposition/equal_area/mod.rs
+++ necsim/impls/no-std/src/decomposition/equal_area/mod.rs
@@ -1,8 +1,9 @@
+use alloc::{boxed::Box, vec::Vec};
 use core::{marker::PhantomData, num::NonZeroU32};
 
 use necsim_core::{
     cogs::Habitat,
-    intrinsics::{ceil, floor, log2},
+    intrinsics::{ceil, log2},
     landscape::{LandscapeExtent, Location},
 };
 
@@ -18,10 +19,9 @@ pub struct EqualAreaDecomposition<H: Habitat> {
     partitions: NonZeroU32,
 
     extent: LandscapeExtent,
-    prev_log2: (u8, u8),
+    morton: (u8, u8),
 
-    offset: u32,
-    offset_next_pow2: u32,
+    indices: Box<[u64]>,
 
     _marker: PhantomData<H>,
 }
@@ -34,51 +34,56 @@ impl<H: Habitat> EqualAreaDecomposition<H> {
     pub fn new(habitat: &H, rank: u32, partitions: NonZeroU32) -> Result<Self, Self> {
         let extent = habitat.get_extent().clone();
 
-        let mut prev_log2_width = Self::prev_log2(extent.width());
-        let mut prev_log2_height = Self::prev_log2(extent.height());
+        let mut indices = Vec::with_capacity(partitions.get() as usize);
 
-        let partitions_log2 = Self::next_log2(partitions.get());
+        let morton_x = Self::next_log2(extent.width());
+        let morton_y = Self::next_log2(extent.height());
 
-        let successful_decomposition = partitions_log2 <= (prev_log2_width + prev_log2_height);
-
-        // Favour partitions with similar width and height
-        while partitions_log2 < (prev_log2_width + prev_log2_height) {
-            #[allow(clippy::if_same_then_else)]
-            if prev_log2_width == 0 {
-                prev_log2_height -= 1;
-            } else if prev_log2_height == 0 {
-                prev_log2_width -= 1;
-            } else if (extent.width() / (0x1_u32 << prev_log2_width))
-                <= (extent.height() / (0x1_u32 << prev_log2_height))
-            {
-                prev_log2_width -= 1;
-            } else {
-                prev_log2_height -= 1;
-            }
+        for location in extent.iter() {
+            indices.push(Self::map_x_y_to_morton(
+                morton_x,
+                morton_y,
+                location.x() - extent.x(),
+                location.y() - extent.y(),
+            ));
         }
 
-        // Calculate the difference between partitions and 2^k
-        let offset = partitions.get() - (0x1_u32 << Self::prev_log2(partitions.get()));
-        let offset_next_pow2 = if offset > 0 {
-            0x1_u32 << Self::next_log2(offset)
-        } else {
-            partitions.get()
-        };
+        indices.sort_unstable();
+
+        let num_indices = indices.len() as u64;
+        let mut last_rank = 0;
+
+        let indices: Vec<u64> = indices
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, index)| {
+                let next_rank = (i as u64) * u64::from(partitions.get()) / num_indices;
+
+                if next_rank == last_rank {
+                    None
+                } else {
+                    last_rank = next_rank;
+
+                    Some(index)
+                }
+            })
+            .collect();
 
         let decomposition = Self {
             rank,
             partitions,
 
             extent,
-            prev_log2: (prev_log2_width, prev_log2_height),
+            morton: (morton_x, morton_y),
 
-            offset,
-            offset_next_pow2,
+            indices: indices.into_boxed_slice(),
 
             _marker: PhantomData::<H>,
         };
 
-        if successful_decomposition {
+        if u64::from(partitions.get())
+            <= (u64::from(habitat.get_extent().width()) * u64::from(habitat.get_extent().height()))
+        {
             Ok(decomposition)
         } else {
             Err(decomposition)
@@ -94,20 +99,31 @@ impl<H: Habitat> EqualAreaDecomposition<H> {
         }
     }
 
-    fn prev_log2(coord: u32) -> u8 {
-        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-        if coord > 1 {
-            floor(log2(f64::from(coord))) as u8
-        } else {
-            0
-        }
-    }
+    fn map_x_y_to_morton(mut morton_x: u8, mut morton_y: u8, mut dx: u32, mut dy: u32) -> u64 {
+        let mut morton_index = 0_u64;
 
-    fn map_coord_to_index(coord: u32, length: u32, length_next_log2: u8) -> u32 {
-        #[allow(clippy::cast_possible_truncation)]
-        {
-            (u64::from(coord) * (0x1_u64 << length_next_log2) / u64::from(length)) as u32
+        let morton = morton_x.min(morton_y);
+
+        for m in 0..morton {
+            morton_index |= u64::from(dx & 0x1_u32) << (m * 2);
+            dx >>= 1;
+
+            morton_index |= u64::from(dy & 0x1_u32) << (m * 2 + 1);
+            dy >>= 1;
         }
+
+        morton_x -= morton;
+        morton_y -= morton;
+
+        if morton_x > 0 {
+            morton_index |= u64::from(dx) << (morton * 2);
+        }
+
+        if morton_y > 0 {
+            morton_index |= u64::from(dy) << (morton * 2 + morton_x);
+        }
+
+        morton_index
     }
 }
 
@@ -126,24 +142,15 @@ impl<H: Habitat> Decomposition<H> for EqualAreaDecomposition<H> {
         "habitat has a matching extent"
     )]
     fn map_location_to_subdomain_rank(&self, location: &Location, habitat: &H) -> u32 {
-        let idx_x = Self::map_coord_to_index(
-            location.x() - self.extent.x(),
-            self.extent.width(),
-            self.prev_log2.0,
-        );
-        let idx_y = Self::map_coord_to_index(
-            location.y() - self.extent.y(),
-            self.extent.height(),
-            self.prev_log2.1,
-        );
+        let mut dx = location.x() - self.extent.x();
+        let mut dy = location.y() - self.extent.y();
 
-        let index = (idx_y << self.prev_log2.0) | idx_x;
+        let morton_index = Self::map_x_y_to_morton(self.morton.0, self.morton.1, dx, dy);
 
-        // Reduce the mapping from [0, 2^(k+1)) to [0, partitions)
-        if index > self.offset_next_pow2 {
-            (index >> 1) + self.offset
-        } else {
-            index
+        #[allow(clippy::cast_possible_truncation)]
+        match self.indices.binary_search(&morton_index) {
+            Ok(index) => (index + 1) as u32,
+            Err(index) => index as u32,
         }
     }
 }
