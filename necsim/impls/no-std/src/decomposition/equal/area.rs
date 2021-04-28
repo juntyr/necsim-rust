75518b0372287c1d0748e11e144fe5a36393c6d6
Moritz Langenstein <ml5717@ic.ac.uk>
2021-04-28 21:18:18 +0100

    (ml5717) Added simple equal weight landscape decomposition

---
+++ necsim/impls/no-std/src/decomposition/equal/area.rs
@@ -0,0 +1,70 @@
+use alloc::vec::Vec;
+use core::{marker::PhantomData, num::NonZeroU32};
+
+use necsim_core::cogs::Habitat;
+
+use super::EqualDecomposition;
+
+impl<H: Habitat> EqualDecomposition<H> {
+    /// # Errors
+    ///
+    /// Returns `Ok(Self)` iff the `habitat` can be partitioned into
+    /// `partitions` by area, otherwise returns `Err(Self)`.
+    pub fn area(habitat: &H, rank: u32, partitions: NonZeroU32) -> Result<Self, Self> {
+        let extent = habitat.get_extent().clone();
+
+        let mut indices = Vec::with_capacity(partitions.get() as usize);
+
+        let morton_x = Self::next_log2(extent.width());
+        let morton_y = Self::next_log2(extent.height());
+
+        for location in extent.iter() {
+            indices.push(Self::map_x_y_to_morton(
+                morton_x,
+                morton_y,
+                location.x() - extent.x(),
+                location.y() - extent.y(),
+            ));
+        }
+
+        indices.sort_unstable();
+
+        let num_indices = indices.len() as u64;
+        let mut last_rank = 0;
+
+        let indices: Vec<u64> = indices
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, index)| {
+                #[allow(clippy::cast_possible_truncation)]
+                let next_rank = ((i as u64) * u64::from(partitions.get()) / num_indices) as u32;
+
+                if next_rank == last_rank {
+                    None
+                } else {
+                    last_rank = next_rank;
+
+                    Some(index)
+                }
+            })
+            .collect();
+
+        let decomposition = Self {
+            rank,
+            partitions,
+
+            extent,
+            morton: (morton_x, morton_y),
+
+            indices: indices.into_boxed_slice(),
+
+            _marker: PhantomData::<H>,
+        };
+
+        if (decomposition.indices.len() + 1) == (partitions.get() as usize) {
+            Ok(decomposition)
+        } else {
+            Err(decomposition)
+        }
+    }
+}
