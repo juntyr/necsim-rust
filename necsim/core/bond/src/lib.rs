f4dde70319edf8fc4db754c56925251bbdde0cf6
Momo Langenstein <momo.langenstein17@imperial.ac.uk>
2021-05-12 10:27:49 +0000

    (ml5717) Fixed necsim core bond file names

--- necsim/core/bond/src/lib.rs
+++ necsim/core/bond/src/lib.rs
@@ -3,16 +3,16 @@
 #![feature(rustc_attrs)]
 #![feature(total_cmp)]
 
+mod closed_unit_f64;
 mod non_negative_f64;
 mod non_zero_one_u64;
 mod partition;
 mod positive_f64;
-mod zero_excl_one_incl_f64;
-mod zero_incl_one_incl_f64;
+mod positive_unit_f64;
 
+pub use closed_unit_f64::ClosedUnitF64;
 pub use non_negative_f64::NonNegativeF64;
 pub use non_zero_one_u64::NonZeroOneU64;
 pub use partition::Partition;
 pub use positive_f64::PositiveF64;
-pub use zero_excl_one_incl_f64::PositiveUnitF64;
-pub use zero_incl_one_incl_f64::ClosedUnitF64;
+pub use positive_unit_f64::PositiveUnitF64;
