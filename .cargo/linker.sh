3a20047225f3497c4dc8cff649c2d561901580fc
Moritz Langenstein <ml5717@ic.ac.uk>
2021-06-24 10:42:48 +0100

    (ml5717) Publish rustdoc to Github Pages

--- .cargo/linker.sh
+++ .cargo/linker.sh
@@ -1,5 +1,5 @@
 if [ ! -f "$(dirname "$0")/target/release/rustcoalescence-linker" ]; then
-    cargo build --release --target-dir "$(dirname "$0")/target" --manifest-path "$(dirname "$0")/../rustcoalescence/linker/Cargo.toml" 2>>/home/ml5717/necsim-rust/linker.out
+    cargo build --release --target-dir "$(dirname "$0")/target" --manifest-path "$(dirname "$0")/../rustcoalescence/linker/Cargo.toml"
 fi
 
 "$(dirname "$0")/target/release/rustcoalescence-linker" "$@"
