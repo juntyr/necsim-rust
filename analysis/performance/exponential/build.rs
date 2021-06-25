b7b085892790c8462a67b2305d7ea5bb755bc018
Moritz Langenstein <ml5717@ic.ac.uk>
2021-06-25 12:23:10 +0100

    (ml5717) Moved the analysis to the analysis branch

--- analysis/performance/exponential/build.rs
+++
@@ -1,7 +0,0 @@
-use ptx_builder::{builder::Builder, error::Result, reporter::CargoAdapter};
-
-fn main() -> Result<()> {
-    let builder = Builder::new("kernel")?;
-
-    CargoAdapter::with_env_var("CUDA_PTX_KERNEL").build(builder);
-}
