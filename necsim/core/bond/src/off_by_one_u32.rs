36ae70ed046501cb338a471aa6c5716734b4076f
Momo Langenstein <momo.langenstein@helsinki.fi>
2022-01-09 10:21:17 +0000

    Added OffByOneU32 helper bond struct

---
+++ necsim/core/bond/src/off_by_one_u32.rs
@@ -0,0 +1,82 @@
+use core::{convert::TryFrom, fmt};
+
+use serde::{Deserialize, Deserializer, Serialize};
+
+#[derive(Debug)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OffByOneU32Error(u64);
+
+impl fmt::Display for OffByOneU32Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{} is not in {{1, .., 2^32}}.", self.0)
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, TypeLayout)]
+#[repr(transparent)]
+#[serde(try_from = "u64", into = "u64")]
+pub struct OffByOneU32(u32);
+
+impl fmt::Display for OffByOneU32 {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&self.get(), fmt)
+    }
+}
+
+impl OffByOneU32 {
+    /// # Errors
+    ///
+    /// Returns `OffByOneU32Error` if not `1 <= value <= 2^32`
+    pub const fn new(value: u64) -> Result<Self, OffByOneU32Error> {
+        match u32::try_from(value.wrapping_sub(1)) {
+            Ok(value) => Ok(Self(value)),
+            Err(_) => Err(OffByOneU32Error(value)),
+        }
+    }
+
+    #[must_use]
+    /// Creates a off-by-one u32 without checking the value.
+    ///
+    /// # Safety
+    ///
+    /// The value must be in {1, .., 2^32}.
+    pub const unsafe fn new_unchecked(value: u64) -> Self {
+        #[allow(clippy::cast_possible_truncation)]
+        Self(value.wrapping_sub(1) as u32)
+    }
+
+    #[must_use]
+    pub const fn get(self) -> u64 {
+        u64::from(self.0) + 1
+    }
+
+    #[must_use]
+    pub const fn add_incl(self, other: u32) -> u32 {
+        other.wrapping_add(self.0)
+    }
+
+    #[must_use]
+    pub const fn add_excl(self, other: u32) -> u32 {
+        other.wrapping_add(self.0).wrapping_add(1)
+    }
+}
+
+impl TryFrom<u64> for OffByOneU32 {
+    type Error = OffByOneU32Error;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        Self::new(value)
+    }
+}
+
+impl From<OffByOneU32> for u64 {
+    fn from(val: OffByOneU32) -> Self {
+        val.get()
+    }
+}
+
+impl<'de> Deserialize<'de> for OffByOneU32 {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        Self::new(u64::deserialize(deserializer)?).map_err(serde::de::Error::custom)
+    }
+}
